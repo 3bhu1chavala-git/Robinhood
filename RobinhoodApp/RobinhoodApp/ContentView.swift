a08fa69d7742aaf24390817829fc8dfa901a521c
3bhu1chavala <3bhu1chavala@Mac.lan>
2025-05-01 22:24:45 +0530

    Project initial set up

---
+++ RobinhoodApp/RobinhoodApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RobinhoodApp
+//
+//  Created by 3bhu1chavala on 01/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RobinhoodApp/RobinhoodApp/RobinhoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobinhoodAppApp.swift
+//  RobinhoodApp
+//
+//  Created by 3bhu1chavala on 01/05/25.
+//
+
+import SwiftUI
+
+@main
+struct RobinhoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
